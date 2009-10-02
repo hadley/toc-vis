d2494cdfc7de274d9452715caeff136bf7accc80
hadley <h.wickham@gmail.com>
2009-10-01 21:01:43 -0500

    Parse json files

--- 3-download-pages.r
+++ 3-download-pages.r
@@ -1,6 +1,9 @@
+library(plyr)
+
 isbn <- scan("isbns.txt", "")
 url <- paste("http://search.barnesandnoble.com///e/", isbn, sep = "")
 
+# Download html file
 for(i in seq_along(isbn)) {
   path <- paste("books/", isbn[i], ".html", sep = "")
   if (file.exists(path)) {
@@ -14,3 +17,14 @@ for(i in seq_along(isbn)) {
   cat(".")
 }
 cat("\n")
+
+# grep -c "name=\"TOC\"" *.html | cut -d: -f2 | sort | uniq -c
+
+
+# Convert html to json
+
+input <- paste("books/", isbn, ".html", sep = "")
+output <- paste("json/", isbn, ".json", sep = "")
+cmd <- paste("parsley extract-toc.json", input, "-o", output)
+
+l_ply(cmd, system)
