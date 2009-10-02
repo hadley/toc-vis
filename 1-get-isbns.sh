3cdab5e633fa07b84bdf5eafdd2cd4a5d59660e5
hadley <h.wickham@gmail.com>
2009-10-01 21:43:11 -0500

    The next 500 each of stat & prob books

--- 1-get-isbns.sh
+++ 1-get-isbns.sh
@@ -5,8 +5,12 @@
 #   * first 500
 
 curl "http://books.barnesandnoble.com/search/results.aspx?SRT=R&WRD=statistics&AREF=1537&DREF=1&SZE=500"  -o statistics.html
+curl "http://books.barnesandnoble.com/search/results.aspx?SRT=R&WRD=statistics&AREF=1537&DREF=1&SZE=500&SAT=501"  -o statistics-2.html
 
 curl "http://books.barnesandnoble.com/search/results.aspx?SRT=R&WRD=probability&AREF=1537&DREF=1&SZE=500"  -o probability.html
+curl "http://books.barnesandnoble.com/search/results.aspx?SRT=R&WRD=probability&AREF=1537&DREF=1&SZE=500&SAT=501"  -o probability-2.html
 
-parsley barnes-and-nobles.json statistics.html > statistics.json
-parsley barnes-and-nobles.json probability.html > probability.json
+parsley extract-url.json statistics.html > statistics.json
+parsley extract-url.json statistics-2.html > statistics-2.json
+parsley extract-url.json probability.html > probability.json
+parsley extract-url.json probability-2.html > probability-2.json
--- 2-extract-isbn.rb
+++ 2-extract-isbn.rb
@@ -1,15 +1,16 @@
 require 'json'
 
-stat = JSON.parse(File.open("statistics.json").read)
-prob = JSON.parse(File.open("probability.json").read)
+json_files = Dir["*.json"] - Dir["extract-*.json"]
+files = json_files.map{|file| JSON.parse(File.open(file).read)}.flatten
 
-books = stat["books"] + prob["books"]
 
 # Basic url: http://search.barnesandnoble.com/title/author/e/isbn
 # But title and author can be omitted and url still works
-isbns = books.map do |book| 
-  book["url"].match("/e/(.*?)/")[1]
-end.uniq
+isbns = files.map do |file| 
+  file["books"].map do |book|
+    book["url"].match("/e/(.*?)/")[1]
+  end
+end.flatten.uniq.sort
 
 File.open("isbns.txt", "w+") do |file|
   isbns.each{|isbn| file.write isbn + "\n"}
